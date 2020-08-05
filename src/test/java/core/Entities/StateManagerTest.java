f5055909cf2a9c1ae5c867b1fe3f79da9907d054
Tommaso Rodani <trodani@sissa.it>
2020-08-05 13:20:21 +0200

    working StateManager test

--- src/test/java/core/Entities/StateManagerTest.java
+++ src/test/java/core/Entities/StateManagerTest.java
@@ -11,6 +11,14 @@ public class StateManagerTest {
 
     private final StateManager gameState = new StateManager(6);
 
+    private void fillBoard() {
+        Board board = gameState.getBoard();
+        board.setCell(new Point(0,0), Piece.x);
+        board.setCell(new Point(1,0), Piece.o);
+        board.setCell(new Point(0,1), Piece.x);
+        board.setCell(new Point(1,1), Piece.o);
+    }
+
     @Test
     public void testFirstPlayer() {
         Player firstPlayer = gameState.getPlayer();
@@ -19,10 +27,11 @@ public class StateManagerTest {
 
     @Test
     public void testGetBoard() {
-        Board empty = gameState.getBoard();
-        gameState.getBoard().setCell(new Point(0,0), Piece.x);
+        Board empty = new Board(6);
+        fillBoard();
         Board filled = gameState.getBoard();
         assertNotEquals(empty,filled);
+
     }
 
     @Test
