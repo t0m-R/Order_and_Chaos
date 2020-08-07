a3d1dd9e3549b39ebca32c6568cb5b999c0a8ea3
Tommaso Rodani <trodani@sissa.it>
2020-08-07 16:38:07 +0200

    fix message, added Readme with Demo image

---
+++ README.md
@@ -0,0 +1,18 @@
+# Order and Chaos
+
+![](https://travis-ci.org/t0m-R/Order_and_Chaos.svg?branch=master&status=passed)
+
+### Authors:
+- **Marco Franzon**
+- **Tommaso Rodani**
+
+![](Demo.PNG)
+
+### About the game
+Order and Chaos is a variant of tic-tac-toe on a 6x6 board where both players can use *X* or *O*.
+Order start first and has to make a strike of 5 either vertically, horizontally, or diagonally.
+If one or more of these streaks exists order wins otherwise chaos wins when the board is full.
+
+### Compile and run
+
+    ./gradlew OrderandChaos --console=plain
--- src/main/java/UI/Messages.java
+++ src/main/java/UI/Messages.java
@@ -6,10 +6,10 @@ public class Messages {
             "/  __ \\| | | | / _ \\ |  _  |/  ___|    / _ \\ | \\ | ||  _  \\   |  _  || ___ \\|  _  \\|  ___|| ___ \\\n"+
             "| /  \\/| |_| |/ /_\\ \\| | | |\\ `--.    / /_\\ \\|  \\| || | | |   | | | || |_/ /| | | || |__  | |_/ /\n"+
             "| |    |  _  ||  _  || | | | `--. \\   |  _  || . ` || | | |   | | | ||    / | | | ||  __| |    /\n"+
-            "| \\__/\\| | | || | | |\\ \\_/ //\\__/ /   | | | || |\\  || |/ /    \\ \\_/ /| |\\ \\ | |/ / | |___ | |\\\n"+
+            "| \\__/\\| | | || | | |\\ \\_/ //\\__/ /   | | | || |\\  || |/ /    \\ \\_/ /| |\\ \\ | |/ / | |___ | |\\ \\\n"+
             " \\____/\\_| |_/\\_| |_/ \\___/ \\____/    \\_| |_/\\_| \\_/|___/      \\___/ \\_| \\_||___/  \\____/ \\_| \\_|\n";
 
-    public final static String instructions ="\t\t\t\tWelcome to Chaos and Order Board game!\n\n"+
+    public final static String instructions ="\t\t\t\t\t\t\tWelcome to Chaos and Order Board game!\n\n"+
             "\t\tIt's a tic-tac-toe variant with a 6x6 board. Both players can use Xs and Os\n"+
             "\t\tOrder has to make a strike of 5 either vertically, horizontally, or diagonally.\n"+
             "\t\tChaos aims to fill the board while preventing Order to win. Order starts first!\n\n";
